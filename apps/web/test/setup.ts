b25353cf05b3e6a3381f29597282f1c88de05402
Quinceweb <helloquincestone@gmail.com>
2026-09-07 17:10:30 -0400

    test(web): provide IntersectionObserver in jsdom

--- apps/web/test/setup.ts
+++ apps/web/test/setup.ts
@@ -11,3 +11,18 @@ Object.defineProperty(window, "matchMedia", {
     dispatchEvent: () => false,
   }),
 });
+
+class IntersectionObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(window, "IntersectionObserver", {
+  writable: true,
+  value: IntersectionObserverMock,
+});
+Object.defineProperty(globalThis, "IntersectionObserver", {
+  writable: true,
+  value: IntersectionObserverMock,
+});
